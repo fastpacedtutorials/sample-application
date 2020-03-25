48624c93ea4572711ed49a977f89a40d84ac656c
pooyan <p.fekrati@hotmail.com>
2020-03-25 16:25:39 -0400

    added test controller to api project

---
+++ WebApi/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Test #1");
+        }
+    }
+}
