826a181588114571d75a14681e9014e1608ef0aa
pooyan <p.fekrati@hotmail.com>
2020-05-10 19:48:11 -0400

    added webjob console app project

---
+++ WebJob/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"WebJob srarted running at {DateTime.Now}");
+            Console.WriteLine($"Hello world from WebJob");
+            Console.WriteLine($"WebJob finished running at {DateTime.Now}");
+
+        }
+    }
+}
